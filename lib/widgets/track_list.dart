56485d7f62377de06beec65b10cd99479825ec6b
Da0hn <gabrielhondacba@gmail.com>
2022-05-23 13:20:01 -0400

    feat: add visual selection of `current track`

--- lib/widgets/track_list.dart
+++ lib/widgets/track_list.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../data/data.dart';
+import '../models/current_track.dart';
 
 class TrackList extends StatelessWidget {
   final List<Song> tracks;
@@ -10,6 +12,10 @@ class TrackList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DataTable(
+      headingTextStyle:
+          Theme.of(context).textTheme.overline!.copyWith(fontSize: 12),
+      dataRowHeight: 54.0,
+      showCheckboxColumn: false,
       columns: const [
         DataColumn(label: Text('TITLE')),
         DataColumn(label: Text('ARTIST')),
@@ -17,21 +23,33 @@ class TrackList extends StatelessWidget {
         DataColumn(label: Icon(Icons.access_time)),
       ],
       rows: tracks.map((track) {
-        return DataRow(cells: [
-          _dataCell(context, track.title),
-          _dataCell(context, track.artist),
-          _dataCell(context, track.album),
-          _dataCell(context, track.duration),
-        ]);
+        final selected = context.watch<CurrentTrack>().selected?.id == track.id;
+
+        final color = selected
+            ? Theme.of(context).colorScheme.secondary
+            : Theme.of(context).iconTheme.color!;
+
+        return DataRow(
+          cells: [
+            _dataCell(context, track.title, TextStyle(color: color)),
+            _dataCell(context, track.artist, TextStyle(color: color)),
+            _dataCell(context, track.album, TextStyle(color: color)),
+            _dataCell(context, track.duration, TextStyle(color: color)),
+          ],
+          selected: selected,
+          onSelectChanged: (_) {
+            context.read<CurrentTrack>().selectTrack(track);
+          },
+        );
       }).toList(),
     );
   }
 
-  DataCell _dataCell(BuildContext context, String label) {
+  DataCell _dataCell(BuildContext context, String label, TextStyle textStyle) {
     return DataCell(
       Text(
         label,
-        style: TextStyle(color: Theme.of(context).iconTheme.color),
+        style: textStyle,
       ),
     );
   }
